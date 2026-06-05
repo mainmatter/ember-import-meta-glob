45ebf644da36a2aa32ff88ca2e579b7a6787291e
Marine Dunstetter <mdunstetter@auditboard.com>
2026-06-05 12:49:13 +0200

    acceptance test passing for classic and Vite: had to adjust the example to not rely on array order

---
+++ test-apps/webpack-app/tests/acceptance/application-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'webpack-app/tests/helpers';
+
+module('Acceptance | application', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function (assert) {
+    await visit('/');
+
+    assert.strictEqual(currentURL(), '/');
+    assert.dom('[data-test="eager-cats"]').hasText('Team eager #cats: Midnight, Watari');
+    assert.dom('[data-test="eager-dogs"]').hasText('Team eager #dogs: Hero, Pirate');
+    assert.dom('[data-test="lazy-rabbits"]').hasText('Team lazy #rabbits: Arthur, Popote');
+    assert.dom('[data-test="lazy-turtles"]').hasText('Team lazy #turtles: Caroline, Rocket');
+  });
+});
