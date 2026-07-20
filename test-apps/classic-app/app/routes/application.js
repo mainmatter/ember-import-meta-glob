6f4e5f2c105e1328d8fad4cbf552e4a0594a9c1a
Nicolas Joyard <nicolas.joyard@mainmatter.com>
2026-07-20 07:24:04 +0200

    test: manual imports() vs generated imports()

--- test-apps/classic-app/app/routes/application.js
+++ test-apps/classic-app/app/routes/application.js
@@ -2,6 +2,12 @@ import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
   async model() {
+    const lazyRabbits = import('../mirage/lazy-fixtures/rabbits');
+
+    const lazyObj = {
+      "path/to/rabbits": () => import('../mirage/lazy-fixtures/rabbits')
+    };
+
     const eagerFixtures = import.meta.glob("../mirage/eager-fixtures/*", { eager: true });
     const lazyFixtures = import.meta.glob("../mirage/lazy-fixtures/*");
 
